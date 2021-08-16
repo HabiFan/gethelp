cd7aee8dbcce36020799df8ab2fde991b6aeb53e
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2021-08-16 23:57:18 +0300

    Add Answer controller with specs

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
+ 
+  describe 'GET #new' do
+   
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new answer to @answer' do
+       expect(assigns(:answer)).to be_a_new(Answer) 
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: answer } }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:answer) { create(:answer) }
+
+    it 'deletes the question' do
+      expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirects to question' do
+      delete :destroy,  params: { id: answer }
+      expect(response).to redirect_to answer.question
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:question) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the amswer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested answers to @answer' do
+        patch :update, params: { id: answer, answer: attributes_for(:answer) }
+        expect(assigns(:answer)).to eq answer
+      end
+
+      it 'changes answer attributes' do
+        patch :update, params: { id: answer, answer: { body: 'new body' } }
+        answer.reload
+
+        expect(answer.body).to eq 'new body'
+      end
+
+      it 'updated answer redirects to question' do
+        patch :update, params: { id: answer, answer: attributes_for(:answer) }
+        expect(response).to redirect_to answer.question
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) } }
+
+      it 'does not change answer' do
+        answer.reload
+
+        expect(answer.body).to eq 'MyText'
+      end
+
+      it 're-renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+end
